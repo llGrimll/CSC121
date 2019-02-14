51e6e18cb341884e1e84979e9f6bde1ffc6636e3
llGrimll <44880996+llGrimll@users.noreply.github.com>
2019-02-14 16:51:44 -0500

    Class activity Feb 14, 2019

---
+++ Feb14Class.py
@@ -0,0 +1,11 @@
+###############################################################################
+#
+# Class activites on February 14th, 2019
+#
+###############################################################################
+
+fruit = ["banana", "oranges", "apples", "kiwis", "grapefruit", "strawberries",
+         "peaches"]
+for i in fruit:
+    if fruit.index(i) % 2 == 0:
+        print(i)
