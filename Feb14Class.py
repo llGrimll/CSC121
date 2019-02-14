6683c015771f3e297f778cab547c69fb0378d330
llGrimll <44880996+llGrimll@users.noreply.github.com>
2019-02-14 17:03:28 -0500

    Finished

--- Feb14Class.py
+++ Feb14Class.py
@@ -6,6 +6,17 @@
 
 fruit = ["banana", "oranges", "apples", "kiwis", "grapefruit", "strawberries",
          "peaches"]
+
+# simple for loop
+for i in fruit:
+    print(i)
+
+# print even indexes
 for i in fruit:
     if fruit.index(i) % 2 == 0:
+        print(i)
+
+# print odd indexes
+for i in fruit:
+    if fruit.index(i) % 2 != 0:
         print(i)
