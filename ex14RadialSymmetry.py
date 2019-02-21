ed819987f71117c7a12263156735895fd7d10c4e
llGrimll <44880996+llGrimll@users.noreply.github.com>
2019-02-21 18:47:22 -0500

    final ex14

--- ex14RadialSymmetry.py
+++ ex14RadialSymmetry.py
@@ -7,28 +7,46 @@
 import turtle
 import random
 
+objects = 12
+length = 50
+
 
 def draw_polygon(t):
     color_list = ['Red', 'Green', 'Blue', 'Brown', 'Aqua', 'Black',
                   'Aquamarine', 'Cyan', 'Crimson']
     clr = color_list[random.randint(0, 8)]
-    length = 50
-    sides = 3
+    global length
+    sides = random.randint(3, 5)
+    t.speed(0)
     t.down()
     t.begin_fill()
     t.color(clr)
     for i in range(sides):
-        t.forward(length)
+        t.forward(length / 2)
         t.left(360 / sides)
+        t.forward(length / 2)
     t.end_fill()
     t.up()
 
 
 def radial_symmetry(t):
-    go to 
-    for i in range():
+    global length
+    global objects
+    draw_polygon(tim)
+    angle = 360 / objects
+    t.goto(0, 20)
+    for i in range(objects):
+        t.forward(200)
+        t.right(90)
+        draw_polygon(tim)
+        t.goto(0, 20)
+        t.left(90 + angle)
+    t.hideturtle()
 
-    t.goto(0, 0)
 
-
+wn = turtle.Screen()
 tim = turtle.Turtle()
+
+radial_symmetry(tim)
+
+wn.exitonclick()
