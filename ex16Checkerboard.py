4f0f4f3d2aad8179b9a1417136a45062dadfa5af
llGrimll <44880996+llGrimll@users.noreply.github.com>
2019-02-26 21:02:52 -0500

    ex16 clean up revamped functions to be more efficient

--- ex16Checkerboard.py
+++ ex16Checkerboard.py
@@ -8,68 +8,55 @@
 ###############################################################################
 
 import turtle
-import random
 
 
+# function for the black squares with space in between
 def drawSquaresRight(t):
     length = 50
-    t.speed(0)
-    # t.pendown()
     t.begin_fill()
+    # for loop to make one square
     for i in range(4):
-        t.color("black")
         t.forward(length)
         t.left(90)
     t.end_fill()
-    t.forward(50)
-    for i in range(4):
-        t.pendown()
-        t.forward(length)
-        t.left(90)
-        t.penup()
-    t.forward(length)
-    # t.penup()
+    t.forward(length * 2)  # skipping the white space creating the white box
 
 
+# similar fuction as drawSquaresRight, changes black and white space
 def drawSecondRow(t):
     length = 50
     t.speed(0)
-    # t.pendown()
-    for i in range(4):
-        t.pendown()
-        t.forward(length)
-        t.left(90)
-        t.penup()
     t.forward(50)
     t.begin_fill()
     for i in range(4):
-        t.color("black")
         t.forward(length)
         t.left(90)
     t.end_fill()
-    t.forward(length)
-    # t.penup()
+    t.forward(length * 2)  # skipping the white space creating the white box
 
 
+# function to draw border
 def borderFunction(t):
+    # go to top left corner
     t.goto(-200, 200)
     t.pendown()
+    # faces turtle down
     t.left(180)
+    # draws square around the edge
     for i in range(4):
         t.left(90)
         t.forward(400)
     t.hideturtle()
 
 
-wn = turtle.Screen()
-geo = turtle.Turtle()
-
-
-def drawRowsSquares(t):
+# putting all functions together, creating all rows and border around
+def drawCheckerboard(t):
+    t.speed(0)
     y = -200
     # number of rows in function
     for i in range(4):
         x = -200
+        # creates black squares in odd rows and moves it up to next odd row
         for i in range(4):
             t.penup()
             t.goto(x, y)
@@ -77,10 +64,10 @@ def drawRowsSquares(t):
             x += 100
         y += 100
     y = -150
+    # creates black squares in even rows and moves it up to next even row
     for i in range(4):
         x = -200
         for i in range(4):
-            t.penup()
             t.goto(x, y)
             drawSecondRow(geo)
             x += 100
@@ -88,4 +75,10 @@ def drawRowsSquares(t):
     borderFunction(geo)
 
 
-drawRowsSquares(geo)
+wn = turtle.Screen()
+geo = turtle.Turtle()
+
+# calls function
+drawCheckerboard(geo)
+
+wn.exitonclick()
