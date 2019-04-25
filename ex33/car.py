fb5d8da1d8533476d30c0d58119091de77353b17
llGrimll <44880996+llGrimll@users.noreply.github.com>
2019-04-25 17:08:33 -0400

    ex33 Object Oriented Programming

---
+++ ex33/car.py
@@ -0,0 +1,38 @@
+###############################################################################
+#
+# ex33 in-class 4/25/19 - OOP
+#
+###############################################################################
+
+
+class Car:
+    """Class takes specifications of new cars."""
+
+    my = "Brand New - Model Year 2020:"
+
+    #  constructor method init
+    def __init__(self, make, model, color, price):
+        """Return the make and model of a new car."""
+        self.make = make
+        self.model = model
+        self.color = color
+        self.price = price
+        # nu += 1
+
+    def __str__(self):
+        """Return make and model of new car."""
+        return f"{Car.my}, {self.make}, {self.model}, {self.color}, {self.price}."
+
+
+class Used(Car):
+    """Specifications of used cars."""
+
+    def __init__(self, year, make, model, color, miles, price):
+        """Add Miles and price to used car class."""
+        super().__init__(make, model, color, price)
+        self.year = year
+        self.miles = miles
+
+    def __str__(self):
+        """Return make and model of used car."""
+        return f"Certified Pre-Owned: {self.year} {self.make} {self.model}, {self.color}, {self.miles} miles, ${self.price}."
---
+++ ex33/main.py
@@ -0,0 +1,12 @@
+from car import Car, Used
+
+c1 = Car("Mercedes", "GL350", "Silver", "60K")
+c2 = Car("Acura", "RTX", "Black", "45K")
+c3 = Car("Chevy", "Malibu", "Blue", "20K")
+c4 = Car("Ford", "F150", "Yellow", "40K")
+u1 = Used("2010", "Honda", "Pilot", "Silver", "26K", "23K")
+
+print(c1.color)
+print(u1.year)
+print(c1)
+print(u1)
